2f15df42d4ea6a68d886543a5df19c3e56138935
Ken Cheng <kenny851017@gmail.com>
2023-07-14 13:20:17 -0700

    Update QueueDatabaseEntry+Query.swift to Fix Literal Casting of Double

--- Sources/QueuesDatabaseHooks/QueueDatabaseEntry+Query.swift
+++ Sources/QueuesDatabaseHooks/QueueDatabaseEntry+Query.swift
@@ -53,7 +53,7 @@ public extension QueueDatabaseEntry {
                                 CASE WHEN CAST(\(ident: "status") AS TEXT)::INTEGER = \(literal: 2) THEN \(literal: 1) ELSE \(literal: 0) END
                             ) AS DOUBLE PRECISION
                         ) / GREATEST(COUNT(\(ident: "id")), 1),
-                        \(literal: 1.0)
+                        \(literal: 1) AS DOUBLE PRECISION
                     ) AS \(ident: "percentSuccess")
                 FROM
                     \(ident: QueueDatabaseEntry.schema)
